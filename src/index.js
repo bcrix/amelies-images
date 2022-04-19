f2fc6bd2ae3e978775408900dfcbb03cba37bd9d
Noaah <88775365+bcrix@users.noreply.github.com>
2022-04-19 02:59:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 const fetch = require("node-fetch")
 
-function image(type) {
+async function image(type) {
     let url = `https://developers.ameliebot.com/api/image-api/${type}`;
     let settings = { method: "Get" };
 
-    fetch(url, settings).then(res => res.json()).then((json) => {
-        console.log(json.url)
+    return fetch(url, settings).then(res => res.json()).then((json) => {
+        return(json.url)
     }).catch(err => {
         return console.log(err)
     })
+
 }
 
 module.exports = {
     image
-};
+}
